10e5c80e0d3068bc4dc78397addccf995a0b030e
humnya_notebook <boysjun@naver.com>
2025-05-21 13:45:41 +0900

    first commit

---
+++ toonflix/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Scaffold(body: Text('이제 시작이다 열심히하자!!')));
+  }
+}
