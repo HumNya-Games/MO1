3a5875a5dbdac3eee07c5604a3441c393a3dbc43
humnya_notebook <boysjun@naver.com>
2025-05-22 13:41:23 +0900

    버튼제작

--- toonflix/lib/main.dart
+++ toonflix/lib/main.dart
@@ -11,8 +11,9 @@ class App extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Color(0xff181818),
         body: Padding(
-          padding: EdgeInsetsGeometry.only(right: 10),
+          padding: EdgeInsetsGeometry.symmetric(horizontal: 15),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Row(
                 mainAxisAlignment: MainAxisAlignment.end,
@@ -30,7 +31,7 @@ class App extends StatelessWidget {
                         ),
                       ),
                       Text(
-                        "환영합니다",
+                        "귀환을 환영합니다",
                         style: TextStyle(
                           color: Colors.white.withOpacity(0.8),
                           fontWeight: FontWeight.w500,
@@ -38,6 +39,47 @@ class App extends StatelessWidget {
                         ),
                       ),
                     ],
+                  ),
+                ],
+              ),
+              SizedBox(height: 120),
+              Text(
+                "Tatal Balance",
+                style: TextStyle(
+                  fontSize: 22,
+                  color: Colors.white.withOpacity(0.8),
+                ),
+              ),
+              SizedBox(height: 2),
+              Text(
+                "\$17,200,000",
+                style: TextStyle(
+                  fontSize: 42,
+                  fontWeight: FontWeight.w900,
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(height: 13),
+              Row(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.amber,
+                      borderRadius: BorderRadius.circular(40),
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(
+                        vertical: 15,
+                        horizontal: 40,
+                      ),
+                      child: Text(
+                        'Transfer',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                    ),
                   ),
                 ],
               ),
